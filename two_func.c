370eaf5ffec7555758da055dd0aa0814cc734fab
CaiJiudll <rzx1021@163.com>
2025-09-08 12:17:14 +0800

    Add files via upload

---
+++ two_func.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//void butler(void);
+//int main(void) {
+//	printf("11111111111\n");
+//	butler();
+//	printf("22222222222222\n");
+//
+//	return 0;
+//}
+//void butler(void) {
+//	printf("hello,world\n");
+//}
